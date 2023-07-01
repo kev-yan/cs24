0cabbba3f5c50f8eddb85d9ab253fde7d26e7271
Kevin Yan <kevinyan@MacBook-Air.local>
2023-06-30 23:11:05 -0700

    test

--- welcome/code/caprot.cpp
+++ welcome/code/caprot.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char* argv[]){
+
+    if(argc == 3){
+        int temp = argc;
+        string test;
+        //cout << argv[2];
+        test = argv[2];
+        cout << temp << endl;
+        cout << test << endl;
+    }
+    else{
+        cout << "please try again" << endl;
+    }
+    
+}
