773c86418b90ff1d0c94c5697d95d55784394e16
Kevin Yan <kevinyan@MacBook-Air.local>
2023-07-01 00:20:38 -0700

    working

--- welcome/code/caprot.cpp
+++ welcome/code/caprot.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <vector>
 using namespace std;
 
+void rightCap(int num, string input){
+    int length = input.length();
+    vector<int> upper;
+    int i;
+    for(i=0; i<length; i++){
+        if(isupper(input[i])){
+            int index = (i+num) % length;
+            upper.push_back(index);
+            input[i] = tolower(input[i]);
+        }
+    }
+    for(i=0;i<upper.size();i++){
+        input[upper.at(i)] = toupper(input[upper.at(i)]);
+    }
+    cout << input << endl;
+
+}
 
 int main(int argc, char* argv[]){
 
     if(argc == 3){
-        int temp = argc;
-        string test;
-        //cout << argv[2];
-        test = argv[2];
-        cout << temp << endl;
-        cout << test << endl;
+        rightCap(stoi(argv[1]), argv[2]);
     }
     else{
         cout << "please try again" << endl;
