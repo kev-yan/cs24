f34919e1a83b5b5b508c98e5b5da44ae5093b1f5
Kevin Yan <kevinyan@MacBook-Air.local>
2023-07-20 18:02:45 -0700

    test

--- chunkylist/MyChunkyList.cpp
+++ chunkylist/MyChunkyList.cpp
@@ -6,4 +6,45 @@ MyChunkyList::MyChunkyList(int chunksize){
     headPtr = nullptr;
     tailPtr = nullptr;
 }
-// TODO: Member Function Implementation
+
+MyChunkyList::~MyChunkyList(){
+    while(headPtr != tailPtr){
+        MyChunkyNode* temp = headPtr->next();
+        headPtr->~MyChunkyNode();
+        delete headPtr;
+        headPtr = temp;
+    }
+    tailPtr->~MyChunkyNode();
+    delete tailPtr;
+}
+
+int MyChunkyList::count() const{
+    int total = 0;
+    MyChunkyNode* temp = headPtr;
+    while(temp != null){
+        total += temp->count();
+        temp = temp->next();
+    }
+    return total;
+}
+
+void MyChunkyList::insert(int index, const std::string& item){
+
+}
+
+std::string& MyChunkyList::lookup(int index){
+
+}
+
+void MyChunkyList::remove(int index){
+
+}
+
+MyChunkyNode* MyChunkyList::head() const{
+    return headPtr;
+}
+
+MyChunkyNode* MyChunkyList::tail() const{
+    return tailPtr;
+}
+
