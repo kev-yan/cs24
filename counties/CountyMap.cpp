89eecac60857f4c7c3bdb79d6a7ca0f49435d0de
Kevin Yan <kevinyan@169-231-23-47.wireless.ucsb.edu>
2023-08-28 15:54:37 -0700

    test

--- counties/CountyMap.cpp
+++ counties/CountyMap.cpp
@@ -1,8 +1,23 @@
 #include "CountyMap.h"
 #include "County.h"
 #include <vector>
+#include <iostream>
 
-CountyMap(vector<County> counties, vector<vector<bool>> adjMatrix) {
+using namespace std;
+
+
+void CountyMap::printMatrix(){
+    for(int i=0; i<adjMatrix.size();i++){
+        for(int j=0; i<adjMatrix.at(i).size(); i++){
+            cout << adjMatrix.at(i).at(j) << " ";
+        }
+        cout << endl;
+    }
+}
+
+CountyMap::CountyMap(vector<County> counties, vector<vector<bool> > adjMatrix) {
+    this->counties = counties;
+    this->adjMatrix = adjMatrix;
     // TODO
 }
 
@@ -15,18 +30,20 @@ CountyMap::~CountyMap() {
 }
 
 County CountyMap::getCounty(int index) {
-    // TODO
+    return counties.at(index);
 }
 
-int getNumCounties() {
-    // TODO
+int CountyMap::getNumCounties() {
+    return counties.size();
 }
 
 bool CountyMap::countyAdjacency(int index1, int index2) {
+    
     // TODO
 }
 
 bool CountyMap::checkValidItinerary(vector<int> itinerary) {
+    //go through each countyId in itenerary and check if there are edges between each county
     // TODO
 }
 
@@ -34,4 +51,7 @@ vector<int> CountyMap::getAdjacentCounties(int index) {
     // TODO
 }
 
+void CountyMap::setEdge(int first, int second){
+    adjMatrix.at(first).at(second) = true;
+}
 // helper functions may follow
--- counties/CountyMap.h
+++ counties/CountyMap.h
@@ -4,15 +4,23 @@
 #include <vector>
 #include "County.h"
 
+using namespace std;
+
 class CountyMap {
 	private:
 		//storage requirement: O(e), e = number of edges between counties
 
 		//add private variables here
+		vector<County> counties;
+		vector<vector<bool> > adjMatrix;
 	public:
 		//constructor
 		//performance: O(n^2), n = number of counties
-		CountyMap(vector<County> counties, vector<vector<bool>> adjacencyMatrix);
+		CountyMap(vector<County> counties, vector<vector<bool> > adjacencyMatrix);
+
+		CountyMap(const CountyMap& other) ;
+
+		~CountyMap();
 
 		//performance: O(1)
 		County getCounty(int countyID);
@@ -28,4 +36,9 @@ class CountyMap {
 
 		//performance: O(n), n = number of counties in the itinerary
 		bool checkValidItinerary(vector<int> itinerary);
-}
+
+		void printMatrix();
+		void setEdge(int first, int second);
+};
+
+#endif
